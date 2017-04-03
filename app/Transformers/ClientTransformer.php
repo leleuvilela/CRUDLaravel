6b611de3e72cff2322760b156a54f785c410bb61
Vinícius Vilela <viniciusvilela19@gmail.com>
2017-04-03 09:00:08 -0300

    update

---
+++ app/Transformers/ClientTransformer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Notebook
+ * Date: 27/03/2017
+ * Time: 09:34
+ */
+
+namespace App\Transformers;
+
+use App\Entities\Project;
+use League\Fractal\TransformerAbstract;
+
+class ProjectTransformer extends TransformerAbstract
+{
+    protected $defaultIncludes = ['members'];
+
+    public function transform(Project $project)
+    {
+        return [
+            'project_id' => (int) $project->id,
+            'client_id' => $project->client_id,
+            'owner_id' => $project->owner_id,
+            'project' => $project->name,
+            'description' => $project->description,
+            'progress' => $project->progress,
+            'status' => $project->status,
+            'due_date' => $project->due_date
+        ];
+    }
+
+    public function includeMembers(Project $project)
+    {
+        return $this->collection($project->members, new ProjectMemberTransformer());
+    }
+}
---
+++ resources/assets/js/controllers/project-note/projectNoteShow.js
@@ -0,0 +1,5 @@
+angular.module('app.controllers')
+    .controller('ProjectNoteListController', ['$scope', 'Client', function ($scope, Client) {
+        $scope.clients = Client.query();
+
+    }]);
